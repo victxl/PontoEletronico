058560278273bbb010f36456c41e476934adef6a
Victor Henrique <65699690+victxl@users.noreply.github.com>
2024-06-06 22:15:26 -0300

    sobre

--- src/main/java/com/pontoeletronico/MainViewController.java
+++ src/main/java/com/pontoeletronico/MainViewController.java
@@ -2,9 +2,17 @@ package com.pontoeletronico;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import util.Alerts;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -27,12 +35,31 @@ public class MainViewController implements Initializable {
         }
     @FXML
         public void onMenuItemSobre(ActionEvent event) {
-            System.out.println("Sobre");
+           loadView("/com/pontoeletronico/SobreView.fxml");
         }
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    private synchronized void  loadView(String absoluteNome) {
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteNome));
+            VBox newVBox = loader.load();
+
+            Scene mainScene = Main.getMainScene();
+            VBox mainVbox = (VBox) ((ScrollPane)mainScene.getRoot()).getContent();
+
+            Node mainMenu = mainVbox.getChildren().get(0);
+            mainVbox.getChildren().clear();
+            mainVbox.getChildren().add(mainMenu);
+            mainVbox.getChildren().addAll(newVBox.getChildren());
+
+        } catch (IOException e) {
+            Alerts.showAlert("IO Exception","ERRO carregando view",e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 }
